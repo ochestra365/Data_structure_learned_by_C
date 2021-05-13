bfd9f4649a523386ede0d3ff771da7a6eb8dfcba
ochestra365 <ochestra365@naver.com>
2021-05-13 13:10:52 +0900

    commit

--- Project_Data_Structure/3-4.c
+++ Project_Data_Structure/3-4.c
@@ -11,8 +11,9 @@ typedef struct {//구조체 정의
 polynomial addPoly(polynomial, polynomial);//출력 1개, 입력 2개 다항식은 다항식으로 출력되니 다항식 변수르 ㄹ쓴다.
 void printPoly(polynomial);
 
-int main(void) {
+int main(void) {//다항식의 계수부분은 1차원 배열이 선언되었다.
 	polynomial A= { 3,{4,3,5,0} };//다항식의 초기화// A는 3차항의 정보를 가지고있다.
+	
 	polynomial B = { 4,{3,1,0,2,1} };//B는 4차항의 정보를 가지고 있다.
 
 	polynomial C;//다항식 A,B를 덧셈을 시행할 값을 저장할 구조체변수
@@ -25,16 +26,18 @@ int main(void) {
 
 polynomial addPoly(polynomial A, polynomial B) {//지금 A와 B의 차수와 계수들을 모두 복사해왔다.
 	polynomial C; //다항식의 덧셈 결과를 저장할 구조체 변수 선언
-	int A_index = 0, B_index = 0, C_index = 0;//인덱스 초기화는 어떤 용도? 반복문을 제어해주기 위한 용도
+	int A_index = 0, B_index = 0, C_index = 0;//인덱스 초기화는 어떤 용도? 반복문을 제어해주기 위한 용도 계수가 저장되어 있는 1차원 배열의 논리적 주소를 지시하기 위한 용도다.
 	int A_degree = A.degree, B_degree = B.degree;//메인에서 복사해온 차수를 초기화된 함수의 지역변수에 대입한다.
 	C.degree = MAX(A.degree, B.degree);//매크로 활용이다. 해당 매크로는 A가 크면 A를 반환하고, 그렇지않으면 B를 반환한다.
 	//그 반환값을 C의 최고 차수에 대입하여 할당했다.
 
-	while (A_index <= A.degree && B_index <= B.degree) {//3단 분기가 설정되어 있는 상태이고, 차수별로 논리지시자가 옮겨간다.
+	//각 차수별로 계산하기 위한 로직이 아래이다.
+	while (A_index <= A.degree && B_index <= B.degree) {//3단 분기가 설정되어 있는 상태이고, 차수별로 논리지시자가 옮겨간다. 인덱스 갑싱 A나 B의 최고차수를 넘어가면 반복문을 빠져나오는 것이다.
 		if (A_degree > B_degree) {//차수의 가능한 조건 분기를 지금 설정해주고 있는 것이다. 
 			C.coef[C_index++] = A.coef[A_index++]+B.coef[B_index++];//해당 코드의계수를 사용하겠다.// 배열을 후위연산으로 시행
 			//구조체를 써서 순차 자료구조로 만들었다.
 			A_degree--;//차수를 하나씩 낮춰서 비교해간다.
+			//그래서 계수부분을 1차원 배열요소로써 불러올 수 있다.
 		}
 		else if (A.degree == B_degree) {
 			C.coef[C_index++] = A.coef[A_index++] + B.coef[B_index++];
