8826933caba704d571fcfcc5dd9373948cb51ffc
ochestra365 <ochestra365@naver.com>
2021-05-15 11:56:30 +0900

    코드는 자연어다 쉽게 생각나는 대로 치면 그게 정답

--- Chapter4/따라쓰는 연결 자료구조.c
+++ Chapter4/따라쓰는 연결 자료구조.c
@@ -33,8 +33,87 @@ void printList(Head* L) {
 		curr = curr->next;
 		if (curr != NULL)printf(", ");
 	}
+	printf(")\n");
+}
+void insertFirstNode(Head* L, char* x) {
+	Node* newNode = (Node*)malloc(sizeof(Node));
+	strcpy(newNode->data, x);
+	newNode->next = L->head;
+	L->head = newNode;
+}
+void insertMiddleNode(Head* L, Node* pre, char* x) {
+	Node* newNode = (Node*)malloc(sizeof(Head));
+	strcpy(newNode->data, x);
+	if (L == NULL) {
+		newNode->next = NULL;
+		L->head = newNode;
+	}
+	else if (pre == NULL) {
+		L->head = newNode;
+	}
+	else {
+		newNode->next = pre->next;
+		pre->next = newNode;
+	}
+}
+void inserLastNode(Head* L, char* x) {
+	Node* newNode;
+	Node* curr;
+	newNode = (Node*)malloc(sizeof(Node));
+	strcpy(newNode->data, x);
+	newNode->next = NULL;
+	if (L->head == NULL) {
+		L->head = newNode;
+		return;
+	}
+	curr = L->head;
+	while (curr->next != NULL)curr = curr->next;
+	curr->next = newNode;
+}
+void deleteNode(Head* L, Node* P) {
+	Node* curr;
+	if (L->head == NULL)return;
+	if (L->head->next == NULL) {
+		free(L->head);
+		L->head = NULL;
+		return;
+	}
+	else if (P == NULL)return;
+	else {
+		curr = L->head;
+		while (curr->next != P) {
+			curr = curr->next;
+		}
+		curr->next = P->next;
+		free(P);
+	}
+}
+Node* searchNode(Head* L, char* x) {
+	Node* temp;
+	temp = L->head;
+	while (temp != NULL) {
+		if (strcmp(temp->data, x) == 0)return temp;
+		else temp = temp->next;
+	}
+	return temp;
+
+}
+void reverse(Head* L) {
+	Node* p;
+	Node* q;
+	Node* r;
+	p = L->head;
+	q = NULL;
+	L = NULL;
+	while (p != NULL) {
+		r = q;
+		q = p;
+		p = p->next;
+		q->next = r;
+	}
+	L->head = q;
 }
 int main() {
-
+	//출력부분
 	return 0;
 }
