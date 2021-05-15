da057dacda172d739f5a03e8991917cecb5b7477
ochestra365 <ochestra365@naver.com>
2021-05-15 12:46:58 +0900

    소스코드 비교하면서 생각을 비교하자

--- Chapter4/대현씨가 만드 소스 코드 따라해보기.c
+++ Chapter4/대현씨가 만드 소스 코드 따라해보기.c
@@ -16,5 +16,37 @@ HeadNode* createHead(void) {
 }
 
 void preInserNode(HeadNode* h, int i) {
+	Node* newNode = (Node*)malloc(sizeof(Node));
+	if (newNode != NULL) {
+		newNode->data = i;
+		newNode->next = h->head;
+		h->head = newNode;
+	}
+}
 
+Node rearInserNode(HeadNode* h, int i) {
+	Node* newNode = (Node*)malloc(sizeof(Node));
+	Node* curr;
+
+	if (newNode != NULL) {
+		newNode->data = i;
+		newNode->next = NULL;
+		curr = h->head;
+		if (h->head == NULL) {
+			h->head = newNode;
+			return;
+		}
+		while (curr->next != NULL) {
+			curr = curr->next;
+		}
+		curr->next = newNode;
+	}
+}
+
+void Print_Node(HeadNode* h) {
+	int i = 1;
+	while (h->head != NULL) {
+		printf("%d번째 노드 값 :%d\n", i++, h->head->data);
+		h->head = h->head->next;
+	}
 }
