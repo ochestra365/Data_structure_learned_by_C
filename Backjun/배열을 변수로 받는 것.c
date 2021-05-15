9900af83ac5f4a1050c8316cce275f15cabd4150
ochestra365 <ochestra365@naver.com>
2021-05-15 16:06:40 +0900

    ㅋ 스스로 푼 백준

--- Backjun/배열을 변수로 받는 것.c
+++ Backjun/배열을 변수로 받는 것.c
@@ -1,23 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main() {
-	int n;
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++) {
-		int* num = (int*)malloc(sizeof(int));
-		int tmp = *num;
-		int sum = 0, count = 0;
-		double avg;
-		scanf("%d", &num);
-		for (int j = 0; j < num; j++) {
-			scanf("%d", num[j]);
-			sum += num[j];
-		}
-		avg = sum / tmp;
-		for (int k = 0; k < num; k++) {
-			if (avg < num[k]) count++;
-		}
-		free(num + i);
-	}
-	return 0;
-}
+//#include <stdio.h>
+//#include <stdlib.h>
+//int main() {
+//	int n;
+//	scanf("%d", &n);
+//	for (int i = 0; i < n; i++) {
+//		int* num = (int*)malloc(sizeof(int));
+//		int tmp = *num;
+//		int sum = 0, count = 0;
+//		double avg;
+//		scanf("%d", &num);
+//		for (int j = 0; j < num; j++) {
+//			scanf("%d", num[j]);
+//			sum += num[j];
+//		}
+//		avg = sum / tmp;
+//		for (int k = 0; k < num; k++) {
+//			if (avg < num[k]) count++;
+//		}
+//		free(num + i);
+//	}
+//	return 0;
+//}
--- Backjun/연습장.c
+++ Backjun/연습장.c
@@ -1,17 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+long long sum(int* a, int n);
+
 int main() {
-	int n;
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++) {
-		int* num = (int*)malloc(sizeof(int));
-		int sum = 0, count = 0;
-		double avg;
-		int arr[num];
-		scanf("%d", &num);
-		for (int j = 0; j < num; j++) {
-			scanf("%d", &arr[j]);
-		}
-	}
+	int ary[5] = { 1,2,3,4,5 };
+	int* pary = ary;
+	int res;
+	res=sum(pary, 3);
+	printf("%d", res);
 	return 0;
 }
+long long sum(int* a, int n) {
+	int res = 0;
+	for (int i = 0; i < n; i++) {
+		res += a[i];
+	}
+	return res;
+}
+
+//a: 합을 구해야 하는 정수 n개가 저장되어 있는 배열(0 ≤ a[i] ≤ 1, 000, 000, 1 ≤ n ≤ 3, 000, 000)
+//n : 합을 구해야 하는 정수의 개수
+//리턴값 : a에 포함되어 있는 정수 n개의 합
