e8390a77da255b5f1f4ae4f3d1743604d3f78b4c
ochestra365 <ochestra365@naver.com>
2021-05-17 17:49:49 +0900

    commit

--- 원형리스트/원형리스트.c
+++ 원형리스트/원형리스트.c
@@ -11,10 +11,10 @@ HeadNode* createHead();
 Node* createNode(HeadNode* phead, int data); 
 void insertFirstNode(HeadNode* phead, Node* newNode);
 void insertLastNode(HeadNode* phead, Node* newNode);
-void printNode(HeadNode* phead);//만들기
+//void printNode(HeadNode* phead);//만들기
 int main() {
 	HeadNode* h = createHead();//헤더 생성
-	Node* A=createNode(h, 10);
+	Node* A = createNode(h, 10);
 	Node* B = createNode(h, 20);
 	Node* C = createNode(h, 30);
 	Node* D = createNode(h, 40);
@@ -28,14 +28,14 @@ HeadNode* createHead() {
 	return h;
 }
 Node* createNode(HeadNode* phead, int data) {
-	Node* newNode = (Node*)malloc(sizeof(Node));
+	Node* newNode = (Node*)malloc(sizeof(Node));//힙 영역에 리스트 크기만큼 저장할 수 있는 만큼 할당받고, 그 값을 리턴 받는다.
 	if (newNode == NULL) {
 		printf("메모리 할당 실패");
 		return;
 	}
 	else {
 		newNode->data = data;
-		newNode->link = NULL;
+		newNode->link = NULL;//만들어진 노드는 그 어떤 것도 가리키지 않은 관계성이 없는 것이다.
 		return newNode;
 	}
 }
@@ -47,7 +47,7 @@ void insertFirstNode(HeadNode* phead, Node* newNode) {//수열의 초항
 	else {
 		newNode->link = phead->head;
 		phead->head = newNode;
-		//phead->head->link = phead->head;
+		phead->head->link = phead->head;
 	}
 }
 void insertLastNode(HeadNode* phead, Node* newNode) {
@@ -61,7 +61,7 @@ void insertLastNode(HeadNode* phead, Node* newNode) {
 		newNode->link = phead->head;//마지막 노드를 가리키면 curr노드를 따로 만들어서 움직일 필요가 없다.
 		while (phead->head != NULL) {
 			//tempNode = tempNode->link;
-			phead->head = phead->head->link;
+			phead->head = phead->head->link;//모든 링크값들이 최종적으로 반복을 빠져나와 반환된다.
 		}
 		//tempNode->link = newNode;
 		//newNode->link = phead->head;
