0217f86e083a083555804374f3af3da4919c58d9
ochestra365 <ochestra365@naver.com>
2021-05-20 11:16:19 +0900

    책의 트리(트리는 검색 조건을 조율하는 것이다)

--- Tree/BinaryTree.c
+++ Tree/BinaryTree.c
@@ -1,67 +1,73 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdlib.h>
-//전위순회 데이터를 먼저 처리.
-typedef struct BtreeNode {
-	char data;
-	struct BtreeNode* left;
-	struct BtreeNode* right;
-}BTNode;
-//데이터를 어느 시점에 처리하느냐에 따라서 정해진다.
-BTNode* makeBTNode(data) {
-	BTNode* newNode = (BTNode*)malloc(sizeof(BTNode));
-	newNode->data = data;
-	newNode->left = NULL;
-	newNode->right = NULL;
-	return newNode;
-}
-void makeLeftTree(BTNode* root,BTNode* sub) //부모 트리의 왼쪽에 자식들을 만들 것이다.
-{
-	if (root->left != NULL)free(root->left);//기존의 왼쪾에 서브 트리가 있으면 삭제하고 새로운 놈으로 연결하겠다.
-	root->left = sub;
-}
-void makeRightTree(BTNode* root, BTNode* sub) 
-{
-	if (root->right != NULL)free(root->right);
-	root->right = sub;
-}
-void preorder(BTNode* root) {//전위순회: 데이터->왼쪽서브->오른쪽 서브
-	if (root == NULL)return;
-	printf("%c\t",root->data);
-	preorder(root->left);
-	preorder(root->right);
-}
-void inorder(BTNode* root) {//중위
-	if (root == NULL)return;
-	inorder(root->left);
-	printf("%c\t", root->data);
-	inorder(root->right);
-}
-void postorder(BTNode* root) {//후위
-	if (root == NULL)return;
-	postorder(root->left);
-	postorder(root->right);
-	printf("%c\t", root->data);
-}
-int main() {
-	BTNode* b1 = makeBTNode('a');
-	BTNode* b2 = makeBTNode('b');
-	BTNode* b3 = makeBTNode('c');
-	BTNode* b4 = makeBTNode('d');
-	BTNode* b5 = makeBTNode('e');
-	BTNode* b6 = makeBTNode('f');
-	BTNode* b7 = makeBTNode('g');
-	BTNode* b8 = makeBTNode('h');
-
-	makeLeftTree(b1, b2);
-	makeRightTree(b1, b3);
-	makeLeftTree(b2, b4);
-	makeRightTree(b2, b5);
-	makeLeftTree(b3, b6);
-	makeRightTree(b3, b7);
-	makeLeftTree(b4, b8);
-
-	preorder(b1);
-	printf("\n");
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+////전위순회 데이터를 먼저 처리.
+//typedef struct BtreeNode {
+//	char data;
+//	struct BtreeNode* left;
+//	struct BtreeNode* right;
+//}BTNode;
+////데이터를 어느 시점에 처리하느냐에 따라서 정해진다.
+//BTNode* makeBTNode(data) {
+//	BTNode* newNode = (BTNode*)malloc(sizeof(BTNode));
+//	newNode->data = data;
+//	newNode->left = NULL;
+//	newNode->right = NULL;
+//	return newNode;
+//}
+//void makeLeftTree(BTNode* root,BTNode* sub) //부모 트리의 왼쪽에 자식들을 만들 것이다.
+//{
+//	if (root->left != NULL)free(root->left);//기존의 왼쪾에 서브 트리가 있으면 삭제하고 새로운 놈으로 연결하겠다.
+//	root->left = sub;
+//}
+//void makeRightTree(BTNode* root, BTNode* sub) 
+//{
+//	if (root->right != NULL)free(root->right);
+//	root->right = sub;
+//}
+//void preorder(BTNode* root) {//전위순회: 데이터->왼쪽서브->오른쪽 서브
+//	if (root == NULL)return;
+//	printf("%c\t",root->data);
+//	preorder(root->left);
+//	preorder(root->right);
+//}
+//void inorder(BTNode* root) {//중위
+//	if (root == NULL)return;
+//	inorder(root->left);
+//	printf("%c\t", root->data);
+//	inorder(root->right);
+//}
+//void postorder(BTNode* root) {//후위
+//	if (root == NULL)return;
+//	postorder(root->left);
+//	postorder(root->right);
+//	printf("%c\t", root->data);
+//}
+//int main() {
+//	BTNode* b1 = makeBTNode('a');
+//	BTNode* b2 = makeBTNode('b');
+//	BTNode* b3 = makeBTNode('c');
+//	BTNode* b4 = makeBTNode('d');
+//	BTNode* b5 = makeBTNode('e');
+//	BTNode* b6 = makeBTNode('f');
+//	BTNode* b7 = makeBTNode('g');
+//	BTNode* b8 = makeBTNode('h');
+//
+//	makeLeftTree(b1, b2);
+//	makeRightTree(b1, b3);
+//	makeLeftTree(b2, b4);
+//	makeRightTree(b2, b5);
+//	makeLeftTree(b3, b6);
+//	makeRightTree(b3, b7);
+//	makeLeftTree(b4, b8);
+//
+//	preorder(b1);
+//	printf("\n");
+//	inorder(b1);
+//	printf("\n");
+//	postorder(b1);
+//	printf("\n");
+//	return 0;
+//}
+////순회하는 방식에 따라 출력되는 방식이 다르다. 데이터의 검색 시점에 많은 비중을 둔 알고리즘이다. 기본은 왼쪽이다.
+////단말노드를 만나서 가장 하위노드부터 처리하는 게 inorder이고, preorder는 root부터 처리하는 것이다. 데이터를 어디에서 처리하냐가 중요한거다. 보통 일반적으로 중회처리를 많이 한다.
---
+++ Tree/이진 탐색 트리의 연산프로그램.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include<stdlib.h>
+typedef char element;
+typedef struct treeNode {
+	char key;
+	struct treeNode* left;
+	struct treeNode* right;
+}treeNode;
+
+treeNode* searchBST(treeNode* root, char x);//이진 탐색 트리에서 키값이 x인 노드의 위치를 탐색하는 연산
+treeNode* insertNode(treeNode* p, char x);//포인터 p가 가리키는 노드와 비교하여 노드 x를 삽입하는 연산
+void deleteNode(treeNode* root, element key);//루트 노드부터 탐색하여 키값과 같은 노드를 찾아 삭제하는 연산
+void displayInorder(treeNode* root); 
+
+int main() {
+
+	return 0;
+}
+
+treeNode* searchBST(treeNode* root, char x) {
+	treeNode* p;
+	p = root;
+	while (p != NULL) {
+		if (x < p->key)p = p->left;
+		else if (x == p->key)return p;
+		else p = p->right;
+	}
+	printf("\n 찾는 키가 없습니다.");
+	return p;
+}
+
+treeNode* inserNode(treeNode* p, char x) {
+	treeNode* newNode;
+	if (p == NULL) {
+		newNode = (treeNode*)malloc(sizeof(treeNode));
+		newNode->key = x;
+		newNode->left = NULL;
+		newNode->right = NULL;
+		return newNode;
+	}
+	else if (x < p->key)p->left = inserNode(p->left, x);
+	else if (x > p->key)p->right = inserNode(p->right, x);
+	else printf("\n이미 같은 키가 있습니다.\n");
+
+	return p;
+}
+void deleteNode(treeNode* root, element key) {
+	treeNode* parent, * p, *succ,* succ_parent;
+	treeNode* child;
+	parent = NULL;
+	p = root;
+	while ((p != NULL) && (p->key != key)) {//삭제할 노드의 위치 탐색
+		parent = p;
+		if (key < p->key)p = p->key;
+		else p = p->right;
+	}
+	if (p == NULL) {//삭제할 노드가 없는 경우
+		printf("\n찾는 키가 이진 트리에 없습니다.");
+		return;
+	}
+	if ((p->left == NULL) && (p->right == NULL)) {//삭제할 노드가 단말 노드인 경우
+		if (parent != NULL) {
+			if (parent != NULL) {
+				if (parent->left == p)parent->left = NULL;
+				else parent->right = NULL;
+			}
+			else root = NULL;
+		}
+	}
+	else if ((p->left == NULL) || (p->right == NULL)) {//삭제할 노드가 자식 노드를 한 개 가진 경우
+		if (p->left != NULL)child = p->left;
+		else child = p->right;
+		if (parent != NULL) {
+			if (parent->left == p)parent->left = child;
+			else parent->right = child;
+		}
+		else root = child;
+	}
+	else {//삭제할 노드가 자식 노드 2 개 가진 경우
+		succ_parent = p;
+		succ = p->left;
+		while (succ->right != NULL) {//왼쪽 서브 트리에서 후계자 찾기
+			succ_parent = succ;
+			succ = succ->right;
+		}
+		if (succ_parent->left == succ)succ_parent->left = succ->left;
+		else succ_parent->right = succ->left;
+		p->key = succ->key;
+		p = succ;
+	}
+	free(p);
+}
+
+void displayInorder(treeNode* root) {
+	if (root) {
+		displayInorder(root->left);
+		printf("%c_", root->key);
+		displayInorder(root->right);
+	}
+}
