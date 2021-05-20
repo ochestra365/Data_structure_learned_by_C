04c0570cab2a4aacd3917ae83cdc9bb3f9946040
ochestra365 <ochestra365@naver.com>
2021-05-20 11:40:43 +0900

    선생님 설명

--- Tree/이진 탐색 트리의 연산프로그램.c
+++ Tree/이진 탐색 트리의 연산프로그램.c
@@ -6,7 +6,9 @@ typedef struct treeNode {
 	struct treeNode* left;
 	struct treeNode* right;
 }treeNode;
-
+//이진 트리는 중복된 값이 있으면 안된다. 부모 노드를 기준으로 왼쪽에는 작은 값, 오른 쪽에는 큰값이 존재해야 한다. 그것이 규칙이다.
+//출력 결과는 순서대로 되게 한다. 중복되는 게 없으면서 부모보다 큰 값은 오른쪽으로 배열된다.
+//노드를 삽입 할 때마다 각 분기와 비교해서 비교 분기마다 작으면 각 분기의 왼쪽, 크면 오른쪽으로 배치한다.
 treeNode* searchBST(treeNode* root, char x);//이진 탐색 트리에서 키값이 x인 노드의 위치를 탐색하는 연산
 treeNode* insertNode(treeNode* p, char x);//포인터 p가 가리키는 노드와 비교하여 노드 x를 삽입하는 연산
 void deleteNode(treeNode* root, element key);//루트 노드부터 탐색하여 키값과 같은 노드를 찾아 삭제하는 연산
@@ -14,7 +16,16 @@ void displayInorder(treeNode* root);
 
 int main() {
 	treeNode* root = NULL;
-	searchBST(root, 'k');
+	searchBST(root, 'a');
+	root=insertNode(root, 'd');
+	insertNode(root, 'e');
+	insertNode(root, 'f');
+	insertNode(root, 'g');
+	insertNode(root, 'a');
+	insertNode(root, 'z');
+	insertNode(root, 'u');
+	insertNode(root, 'y');
+	displayInorder(root);
 	return 0;
 }
 
