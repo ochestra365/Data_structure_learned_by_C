7e7484279d6d3fc891b666081fade641faa26c4d
ochestra365 <ochestra365@naver.com>
2021-06-24 10:00:42 +0900

    모른다 빠삭하게 다시 코드리뷰할것

--- Chapter7/이진탐색트리.c
+++ Chapter7/이진탐색트리.c
@@ -23,7 +23,7 @@ treeNode* searchBST(treeNode* root, char x) {
 //포인터 p가 가리키는 노드와 비교하여 노드 x를 삽입하는 연산
 treeNode* insertNode(treeNode* p, char x) {
 	treeNode* newNode;
-	if (p == NULL) {
+	if (p == NULL) {//공백리스트라면
 		newNode = (treeNode*)malloc(sizeof(treeNode));
 		newNode->key = x;
 		newNode->left = NULL;
@@ -32,7 +32,7 @@ treeNode* insertNode(treeNode* p, char x) {
 	}
 	else if (x < p->key) p->left = insertNode(p->left, x);
 	else if (x > p->key)p->right = insertNode(p->right, x);
-	else pritntf("\n이미 같은 키가 있습니다.!\n");
+	else printf("\n이미 같은 키가 있습니다.!\n");
 
 	return p;
 }
@@ -41,42 +41,114 @@ void deleteNode(treeNode* root, element key) {
 	treeNode* parent, * p, * succ, * succ_parent;
 	treeNode* child;
 	parent = NULL;
-	p = root;
+	p = root;//루트에서 탐색하는 노드다.
 	while ((p != NULL) && (p->key != key)) {//삭제할 노드의 위치 탐색
 		parent = p;
 		if (key < p->key) p = p->left;
 		else p = p->right;
 	}
-}
-
-//삭제할 노드가 없는 경우
-if (p == NULL) {
-	printf("\n찾는 키가 이진 트리에 없습니다!!!");
-	return;
-}
-//삭제할 노드가 단말 노드인 경우
-if ((p->left == NULL) && (p->right == NULL)) {
-	if (parent != NULL) {
-		if (parent->left == p) parent->left = NULL;
-		else parent->right = NULL;
+	//삭제할 노드가 없는 경우
+	if (p == NULL) {
+		printf("\n찾는 키가 이진 트리에 없습니다!!!");
+		return;
 	}
-	else root = NULL;
-}
-//삭제할 노드가 자식 노드를 한 개 가진 경우
-else if ((p->left == NULL) || (p->right == NULL)) {
-	if (p->left != NULL)child = p->left;
-	else child = p->right;
-
-	if (parent != NULL) {
-		if (parent->left == p)parent->left = child;
-		else parent->right = child;
+	//삭제할 노드가 단말 노드인 경우
+	if ((p->left == NULL) && (p->right == NULL)) {
+		if (parent != NULL) {
+			if (parent->left == p) parent->left = NULL;
+			else parent->right = NULL;
+		}
+		else root = NULL;
 	}
-	else root = child;
+	//삭제할 노드가 자식 노드를 한 개 가진 경우
+	else if ((p->left == NULL) || (p->right == NULL)) {
+		if (p->left != NULL)child = p->left;
+		else child = p->right;
+
+		if (parent != NULL) {
+			if (parent->left == p)parent->left = child;
+			else parent->right = child;
+		}
+		else root = child;
+	}
+
+	//삭제할 노드가 자식 노드를 두 개 가진 경우
+	else {
+		succ_parent = p;
+		succ = p->left;
+		while (succ->right != NULL) {//왼쪽 서브 트리에서 후계자 찾기
+			succ_parent = succ;
+			succ = succ->right;
+		}
+		if (succ_parent->left == succ)succ_parent->left = succ->left;
+		else succ_parent->right = succ->left;
+		p->key = succ->key;
+		p = succ;
+	}
+	free(p);
+}
+void displayInorder(treeNode* root) {
+	if (root) {
+		displayInorder(root->left);
+		printf("%c_", root->key);
+		displayInorder(root->right);
+	}
+}
+void menu() {
+	printf("\n*------------------*");
+	printf("\n\t1 : 트리 출력");
+	printf("\n\t2 : 문자 삽입");
+	printf("\n\t3 : 문자 삭제");
+	printf("\n\t4 : 문자 검색");
+	printf("\n\t5 : 종료");
+	printf("\n*------------------*");
+	printf("\n메뉴 입력>>");
 }
 
-//삭제할 노드가 자식 노드를 두 개 가진 경우
-else {
-	succ_parent = p;
-	succ = p->left;
-	while(succ->)
+int main() {
+	treeNode* root = NULL;
+	treeNode* foundedNode = NULL;
+	char choice, key;
+
+	//[그림 7-43]과 같은 초기 이진 탐색 트리를 구성하고
+	//노드 G를 루트 노드 포인터 root로 지정
+	root = insertNode(root, 'G');
+	insertNode(root, 'I');
+	insertNode(root, 'H');
+	insertNode(root, 'D');
+	insertNode(root, 'B');
+	insertNode(root, 'M');
+	insertNode(root, 'N');
+	insertNode(root, 'A');
+	insertNode(root, 'J');
+	insertNode(root, 'E');
+	insertNode(root, 'Q');
+	while (1) {
+		menu();
+		scanf(" %c", &choice);
+		switch (choice - '0')
+		{
+		case1: printf("\t[이진 트리 출력] ");
+			displayInorder(root); printf("\n");
+			break;
+		case2: printf("삽입할 문자를 입력하세요 : ");
+			scanf(" %c", &key);
+			insertNode(root, key);
+			break;
+		case3:printf("삭제할 문자를 입력하세요 : ");
+			scanf(" %c", &key);
+			deleteNode(root, key);
+			break;
+		case4: printf("찾을 문자를 입력하세요 : ");
+			scanf(" %c", &key);
+			foundedNode = searchBST(root, key);
+			if (foundedNode != NULL)
+				printf("\n %c를 찾았습니다. \n", foundedNode->key);
+			else printf("\n 문자를 찾지 못했습니다. \n");
+			break;
+		case5:return 0;
+		default: printf("없는 메뉴이니다. 메뉴를 다시 선택하세요!\n");
+			break;
+		}
+	}
 }
