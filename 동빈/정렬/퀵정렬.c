a5c80d97fb0062854f249f77e2fe0b8595697c83
ochestra365 <ochestra365@naver.com>
2021-07-01 14:44:08 +0900

    외쳐 갓동빈!

---
+++ 동빈/정렬/퀵정렬.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#define SWAP(A,B,temp)((tmpe)=(A),(A)=(B),(B)=(A))
+
+int number = 10;
+int data[] = { 1,10,5,8,7,6,4,3,2,9 };
+
+void show() {
+	int i;
+	for (i = 0; i < number; i++) {
+		printf("%d", data[i]);
+	}
+}
+
+void quickSort(int* data, int start, int end) {
+	if (start >= end) {//원소 1개인 경우 그대로 두기.
+		return;
+	}
+	int key = start;//키는 첫 번쨰 원소
+	int i = start + 1, j = end, temp;
+	while (i <= j) {//엇갈릴 때까지 반복
+		while (i <= end && data[i] <= data[key]) {//키 값보다 큰 값을 만날때까지 
+			i++;
+		}
+		while (j > start && data[j] >= data[key]) {//키 값보다 작은 값을 만날때까지
+			j--;
+		}
+		if (i > j) {//현재 엇갈린 상태면 키 값과 교체
+			SWAP(data[i], data[j], temp);
+		}
+	}
+	quickSort(data, start, j - 1);
+	quickSort(data, j + 1, end);
+}
+int main(void) {
+	quickSort(data, 0, number - 1);//배열이니까 포인터로 지시
+	show();
+	return 0;
+}
+//퀵정렬의 평균 시간 복잡도는 O(N*logN), 최악 시간 복잡도는 O(N^2)
