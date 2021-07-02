1cca1043802332889655ca43ec52779346271339
ochestra365 <ochestra365@naver.com>
2021-07-02 12:37:25 +0900

    병합정렬은 모르게따

--- 동빈/정렬/병합정렬.c
+++ 동빈/정렬/병합정렬.c
@@ -1,1 +1,57 @@
-//일단 반으로 나누고 나중에 합쳐서 정렬하면 어떨까?
+//일단 반으로 나누고 나중에 합쳐서 정렬하면 어떨까? -->합치는 순간에 정렬수행
+#include <stdio.h>
+int number = 8;
+
+int size;
+int sorted[8];//정렬 배열은 반드시 전역 변수로 설정
+int count = 0;
+
+void merge(int a[], int m, int middle, int n) {
+	int i = m;
+	int j = middle + 1;
+	int k;
+	//작은 순서대로 배열에 삽입
+	while (i <= middle && j <= n) {
+		if (a[i] <= a[j]) {
+			sorted[k] = a[i];
+		}
+		else {
+			sorted[k] = a[j];
+			j++;
+		}
+		k++;
+	}
+	//남은 데이터도 삽입
+	if (i > middle) {
+		for (int t = j; t <= n; t++) {
+			sored[k] = a[t];
+			k++;
+		}
+	}
+	else {
+		for (int t = i; t <= middle; t++) {
+			sorted[k] = a[t];
+			k++;
+		}
+	}
+	//정렬된 배열을 삽입
+	for (int t = m; t <= n; t++) {
+		a[t] = sorted[t];
+	}
+}
+void mergeSort(int a[], int m, int n) {
+	//이외의 경우는 크기가 1개인 경우
+	if (m < n) {
+		int middle = (m + n) / 2;
+		mergeSort(a, m, middle);
+		mergeSort(a, middle + 1, n);
+		merge(a, m, middle, n);
+	}
+}
+int main(void) {
+	int array[number] = { 7,6,5,8,3,5,9,1 };
+	mergeSort(array, 0, number - 1);
+	for (int i = 0; i < number; i++) {
+		printf("%d", array[i]);
+	}
+}
