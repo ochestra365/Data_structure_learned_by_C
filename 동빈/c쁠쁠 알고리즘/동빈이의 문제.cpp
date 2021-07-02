07da1a267d3c64ef52bff8d7c9d7258801f93ee7
ochestra365 <ochestra365@naver.com>
2021-07-02 14:03:41 +0900

    short coding은 미덕이다

---
+++ 동빈/c쁠쁠 알고리즘/동빈이의 문제.cpp
@@ -0,0 +1,27 @@
+//벡터와 페어만을 이용해서 풀 수 있다. Pair안에 pair가지고 있어서 풀수 있다.
+#include <iostrinm>
+#include <algorithm>
+#include <vector>
+using namespace std;
+bool compare(pair<string, pair<int, int>>a,// 함수에 대한 불값을 넣어준다.
+	pair<string, pair<int, int>>b) {
+	if (a.second.first == b.second.first) {// 두번쨰 필드에 속한 첫번쨰 값
+		return a.second.second > b.second.second;//더 어린학생이 우선순위
+	}
+	else {
+		return a.second.first > b.second.first;
+	}
+}
+int main(void) {
+	vector<pair<string, pair<int, int>>> v;// 페어 안에 페어가 들어가 있다.
+	v.push_back(par<string, pair<int, int>>("나동빈", pair<int, int>(90, 19961222)));
+	v.push_back(par<string, pair<int, int>>("이태일", pair<int, int>(97, 19931518)));
+	v.push_back(par<string, pair<int, int>>("박한울", pair<int, int>(95, 19961222)));
+	v.push_back(par<string, pair<int, int>>("이상욱", pair<int, int>(90, 19940425)));
+	v.push_back(par<string, pair<int, int>>("강종구", pair<int, int>(88, 19990202)));
+	sort(v.begin(), v.end(),compare);//한개의 정렬 기준을 알려줘야 한다.
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i].second << '';
+	}
+	return 0;
+}
